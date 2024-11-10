7caa9c8b7261f1e49cc5ecddc7bc5d5dffa7a26f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-10 07:18:36 +0530

    Update Footer.jsx

--- src/components/NavBar/Footer.jsx
+++ src/components/NavBar/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
                     <p className="py-4 -rotate-6 sign-font text-2xl font-bold">Jehan Weerasuriya</p>
                   </div>
 
-                  <div className="text-center">
+                  <div className="text-center md:my-0 my-8">
                     <h1 className="">
                     My photography is a celebration of everyday life through authentic, unscripted moments captured on the streets. I seek to uncover the beauty, character, and stories often hidden within urban landscapes. Each shot invites viewers to pause, reflect, and see the familiar in a new light. My work explores themes of connection, solitude, and the raw energy of city life. Through my lens, I aim to reveal the extraordinary within the ordinary.
                     </h1>
